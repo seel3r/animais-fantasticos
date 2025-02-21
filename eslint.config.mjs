f4d2a59a8068432d102348f51db63895fdbbb3b6
Sérgio Seeler <sergioseeler@gmail.com>
2025-02-21 20:03:51 -0300

    primeiro commit

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      // 'no-console': 'error'
+      indent: ["error", 2],
+      "keyword-spacing": "error",
+      "no-multiple-empty-lines": "error",
+      "eol-last": ["error", "always"],
+      semi: ["error", "always"],
+      "no-trailing-spaces": "error",
+      "operator-assignment": "error",
+      "no-inner-declarations": [
+        "error",
+        "functions",
+        { blockScopedFunctions: "disallow" },
+      ],
+    },
+  },
+  pluginJs.configs.recommended,
+];
---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,9 @@
+export default function initFetchBitcoin() {
+  fetch(`https://blockchain.info/ticker`)
+    .then((r) => r.json())
+    .then((bitcon) => {
+      const btcPreco = document.querySelector(".btc-preco");
+      btcPreco.innerText = (1000 / bitcon.BRL.sell).toFixed(4);
+    })
+    .catch((erro) => console.log(Error(erro)));
+}
