762573192b3cb3a80feae47f9cfad3943ed2e405
Sérgio Seeler <sergioseeler@gmail.com>
2025-02-27 20:56:47 -0300

    fetch-bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,8 +1,8 @@
-export default function initFetchBitcoin() {
-  fetch(`https://blockchain.info/ticker`)
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then((r) => r.json())
     .then((bitcon) => {
-      const btcPreco = document.querySelector(".btc-preco");
+      const btcPreco = document.querySelector(target);
       btcPreco.innerText = (1000 / bitcon.BRL.sell).toFixed(4);
     })
     .catch((erro) => console.log(Error(erro)));
